dca37a45f60474fd6834fa6f8e730a71f9987b5d
Wender Mata <bliteti.wender@unidas.com.br>
2020-08-21 22:49:33 -0300

    commit

---
+++ ServiceBus/Program.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServiceBus
+{
+    class Program
+    {
+        const string QueueConnectionString = "Endpoint=sb://projetofiap.servicebus.windows.net/;SharedAccessKeyName=ProductPolicy;SharedAccessKey=fWcvvn31juAbBwvn6rIZyiS/NPHhI1DlNDRG/0yKyGE=";
+        const string QueuePath = "productchanged";
+        static IQueueClient _queueClient;
+
+        private static void Main(string[] args)
+        {
+
+            //SendMessagesAsync().GetAwaiter().GetResult();
+            //Console.WriteLine("messages were sent");
+            //Console.ReadLine();
+
+            if (args.Length <= 0 || args[0] == "sender")
+            {
+                SendMessagesAsync().GetAwaiter().GetResult();
+                Console.WriteLine("messages were sent");
+            }
+            else if (args[0] == "receiver")
+            {
+                ReceiveMessagesAsync().GetAwaiter().GetResult();
+                Console.WriteLine("messages were received");
+            }
+            else
+                Console.WriteLine("nothing to do");
+
+            Console.ReadLine();
+
+        }
+
+        private static async Task SendMessagesAsync()
+        {
+            _queueClient = new QueueClient(QueueConnectionString, QueuePath);
+            var messages = "Hi,Hello,Hey,How are you,Be Welcome"
+                .Split(',')
+                .Select(msg =>
+                {
+                    Console.WriteLine($"Will send message: {msg}");
+                    return new Message(Encoding.UTF8.GetBytes(msg));
+                })
+                        .ToList();
+            await _queueClient.SendAsync(messages);
+            await _queueClient.CloseAsync();
+        }
+
+        private static async Task ReceiveMessagesAsync()
+        {
+            _queueClient = new QueueClient(QueueConnectionString, QueuePath);
+            _queueClient.RegisterMessageHandler(MessageHandler,
+                new MessageHandlerOptions(ExceptionHandler) { AutoComplete = false });
+            Console.ReadLine();
+            await _queueClient.CloseAsync();
+        }
+
+        private static Task ExceptionHandler(ExceptionReceivedEventArgs exceptionArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionArgs.Exception}.");
+            var context = exceptionArgs.ExceptionReceivedContext;
+            Console.WriteLine($"Endpoint:{context.Endpoint}, Path:{context.EntityPath}, Action:{context.Action}");
+            return Task.CompletedTask;
+        }
+
+        private static async Task MessageHandler(Message message, CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"Received message:{Encoding.UTF8.GetString(message.Body)}");
+            await _queueClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+    }
+}
